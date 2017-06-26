f6679f7da0421a0ce5a97b10203b1df6dc06a3a9
Joshua Fromm <jwfromm@cs.washington.edu>
2017-06-26 14:47:57 -0700

    nearly working custom c++ ops

---
+++ workspace/custom_op/zero_out.h
@@ -0,0 +1,9 @@
+#ifndef ZERO_OUT_H_
+#define ZERO_OUT_H_
+
+template <typename Device, typename T>
+struct ZeroOutFunctor {
+    void operator()(const Device& d, int size, const T* in, T* out);
+};
+
+#endif

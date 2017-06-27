4c8dab456edcfb88019adf562c2700245f45540e
Joshua Fromm <jwfromm@cs.washington.edu>
2017-06-26 17:51:16 -0700

    updated build script

--- tensorflow/core/user_ops/build_command.sh
+++ tensorflow/core/user_ops/build_command.sh
@@ -1,2 +1,2 @@
-bazel build -c opt --config=cuda //tensorflow/workspace/custom_op:binarize.so --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0"
-bazel build -c opt --config=cuda //tensorflow/workspace/custom_op:multibit.so --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0"
+bazel build -c opt --config=cuda //tensorflow/core/user_ops:binarize.so --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0"
+bazel build -c opt --config=cuda //tensorflow/core/user_ops:multibit.so --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0"
